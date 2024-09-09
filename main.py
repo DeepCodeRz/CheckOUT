cd632fd941ebf950d28167355eb3a6af1779b748
DeepoweRs <bedirhan_kurt_@outlook.com>
2024-09-09 19:35:40 +0200

    style update

--- main.py
+++ main.py
@@ -77,4 +77,4 @@ def options():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5002)
+    app.run(debug=True, port=5003)
--- static/script.js
+++ static/script.js
@@ -1,6 +1,6 @@
 let lat;
 let lng;
-let map = L.map('map').setView([51.505, -0.09], 13);
+let map = L.map('map').setView([40.999, 29], 10);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
